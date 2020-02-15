291752c756482774d80bb3c4cba03e81eb30fa32
Allen Zhao <allenzhao87.cn@gmail.com>
2020-02-15 19:22:55 +0800

    20200215练习try except sample

--- tryException.py
+++ tryException.py
@@ -1,4 +1,14 @@
 try:
-    print(10 / 0)
+    print(10 / "t")
+# except:
+#     print('wrong')
+# except ArithmeticError:
+#     print("you can't divide 0")
+
+except ArithmeticError as e:
+    print(e)
+except TypeError as e:
+    print(e, "\n", "字符不能与数字运算")
 except:
-    print('wrong')
+    print("something is wrong")
+
