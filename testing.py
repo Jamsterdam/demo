821b7a13e27d0f3b464348f4da17431442c7a696
Allen Zhao <allenzhao87.cn@gmail.com>
2020-02-16 08:10:06 +0800

    20200215练习try except sample

--- testing.py
+++ testing.py
@@ -1,5 +1,19 @@
-import inheritanceSample as inhs
+# import inheritanceSample as inhs
+#
+# bho = inhs.Clients('BHO', 'Thailand')
+# bho.status()
+#
+# # this is to import other files
 
-bho = inhs.Clients('BHO', 'Thailand')
-bho.status()
+file = open("allen.txt")
+content = file.read()
+print(content)
+file.close()
+print("\n")
+# open and close a file
+
+with open('C:/Users/Administrator/Desktop/haha.txt') as f:
+    print(f.read())
+# 用with open语句打开，并读文件，
+# question: 用with op en语句打开文件，为什么不需要close?
 
